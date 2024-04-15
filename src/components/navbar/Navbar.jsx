4834d6145aa0bbb1e4d054a492a73a0742b9f213
bartek-korzen <bartoszkorzen.gte@gmail.com>
2024-04-15 22:50:05 +0200

    (change): Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function Navbar() {
-   return (
-    <>
-    
-    </>
-   )
+export default function Navbar({ onRangeChange }) {
+  const [startRange, setStartRange] = useState(''); // Filtrowanie od ilości znaków
+  const [endRange, setEndRange] = useState(''); // Filtrowanie do ilości znaków
+
+  const handleStartChange = (event) => {
+    setStartRange(event.target.value);
+    onRangeChange(event.target.value, endRange);
+  };
+
+  const handleEndChange = (event) => {
+    setEndRange(event.target.value);
+    onRangeChange(startRange, event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Wybierz zakres znaków jakie mają zawierać posty:</h2>
+      {/* Sekcja wyboru początkowego zakresu */}
+      <label>
+        Od:
+        <input type="number" value={startRange} onChange={handleStartChange} />
+      </label>
+
+      {/* Sekcja wyboru końcowego zakresu */}
+      <label>
+        Do:
+        <input type="number" value={endRange} onChange={handleEndChange} />
+      </label>
+    </div>
+  );
 }
