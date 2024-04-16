925f6163b655a5d99017766ba01242ee30eee7db
WojteGK <93033932+WojteGK@users.noreply.github.com>
2024-04-16 12:13:53 +0200

    (fix): filtering posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import PostSection from './components/PostSection/PostSection'
+// src/App.jsx
+
+import React, { useState, useEffect } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import PostSection from './components/PostSection/PostSection';
+import PostDisplaySettings from './components/PostSection/PostDisplaySettings/PostDisplaySettings';
 
 function App() {
-  const [postCount, setPostCount] = useState(10); // domyślna wartość
+  const [postCount, setPostCount] = useState(10);
+  const [range, setRange] = useState({ start: '', end: '' });
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts/')
+      .then(response => response.json())
+      .then(data => setPosts(data));
+  }, []);
+
+  const handleRangeChange = (start, end) => {
+    setRange({ start: start ? parseInt(start) : '', end: end ? parseInt(end) : '' });
+  };
 
   return (
     <div className='App'>
-      <Navbar></Navbar>
-      <PostSection postCount={postCount}/>
+      <Navbar onRangeChange={handleRangeChange} />
+      <PostDisplaySettings setPostCount={setPostCount} />
+      <PostSection posts={posts} postCount={postCount} range={range} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
